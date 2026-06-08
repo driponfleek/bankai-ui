38c3de6cb1c8cabadd61152a6fdbf4e0d0191956
Daniel Yearwood <subscriptions@dyearwood.com>
2026-06-07 21:00:02 -0400

    chore: Migrate Stylelint to current version
    Closes #71

--- packages/linting/stylelint-config/src/stylelint.config.js
+++ packages/linting/stylelint-config/src/stylelint.config.js
@@ -1,14 +1,19 @@
 /* eslint-disable no-null/no-null */
+
 // Rules from:
 // - Stylelint: https://stylelint.io/user-guide/rules/list
 // - Stylelint-SCSS: https://github.com/stylelint-scss/stylelint-scss#readme
+
 const classNamePattern =
     '^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-z0-9]+(?:-[a-z0-9]+)*(?:__[a-z0-9]+(?:-[a-z0-9]+)*)?(?:--[a-z0-9]+(?:-[a-z0-9]+)*)?(?:\\[.+\\])?$';
-module.exports = {
+
+export default {
+    plugins: ['@stylistic/stylelint-plugin'],
     extends: 'stylelint-config-standard-scss',
     rules: {
         'at-rule-no-unknown': null,
         'at-rule-empty-line-before': null,
+        'block-no-redundant-nested-style-rules': null,
         'color-function-notation': null,
         'declaration-empty-line-before': 'never',
         'declaration-no-important': true,
@@ -16,10 +21,7 @@ module.exports = {
             'lower',
             { ignoreFunctions: ['/min/i', '/max/i'] },
         ],
-        indentation: 4,
-        'max-line-length': null,
         'no-invalid-double-slash-comments': null,
-        'scss/at-import-partial-extension': 'always',
         'scss/at-rule-conditional-no-parentheses': null,
         'scss/at-rule-no-unknown': [
             true,
@@ -43,6 +45,7 @@ module.exports = {
         ],
         'scss/dollar-variable-empty-line-before': 'never',
         'scss/double-slash-comment-empty-line-before': null,
+        'scss/load-partial-extension': null,
         'scss/percent-placeholder-pattern': null,
         'selector-max-id': 0,
         'selector-class-pattern': [
@@ -51,9 +54,10 @@ module.exports = {
                 resolveNestedSelectors: true,
             },
         ],
-        'selector-list-comma-newline-after': 'always-multi-line',
-        'string-quotes': 'single',
         // https://github.com/stylelint/stylelint/issues/5863
         'value-keyword-case': ['lower', { camelCaseSvgKeywords: true }],
+        '@stylistic/indentation': 4,
+        '@stylistic/string-quotes': 'single',
+        '@stylistic/selector-list-comma-newline-after': 'always-multi-line',
     },
 };
